2ee908b1adad13117c7ccbc7238807cdaa115fea
Lojj Quang <52100910@student.tdtu.edu.vn>
2023-11-03 15:27:48 +0700

    Login/Sign Up

---
+++ finalProject/project/src/main/java/com/example/project/Services/UserService.java
@@ -0,0 +1,15 @@
+package com.example.project.Services;
+
+import com.example.project.Models.User;
+
+public interface UserService {
+
+    public User saveUser(User user, String url);
+
+    public void removeSessionMessage();
+
+    public void sendEmail(User user, String path);
+
+    public boolean verifyAccount(String verificationCode);
+
+}
